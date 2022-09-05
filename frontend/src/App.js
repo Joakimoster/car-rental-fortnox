47c5c282bc3e37d7346c5d825751b0a0988957a6
Joakim Österberg <Joakimosterberg@hotmail.com>
2022-09-05 22:02:41 +0200

    Minor adjustments to navigation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,7 @@ import {
 } from 'react-router-dom'
 import FormView from './views/FormView'
 import TableView from './views/TableView';
+import NoPage from "./components/exception/NoPage"
 import Navbar from './components/navbar/Navbar';
 
 function App() {
@@ -15,8 +16,10 @@ function App() {
 		<BrowserRouter>
 			<Navbar />
 			<Routes>
+				<Route path="/" element = {<Navigate to ="/addcar"/>}></Route>
 				<Route path="/addcar" element={<FormView />}></Route>
 				<Route path="/cars" element={<TableView />}></Route>
+				<Route path="*" element = { <NoPage/> } ></Route>
 			</Routes>
 		</BrowserRouter>
 	);
--- frontend/src/components/form/Datepicker.js
+++ frontend/src/components/form/Datepicker.js
@@ -1,7 +1,5 @@
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
-import subDays from "date-fns/subDays";
-import addDays from "date-fns/addDays";
 
 export const Datepicker = ({ inputProps, onChange, value }) => {
 
@@ -11,22 +9,13 @@ export const Datepicker = ({ inputProps, onChange, value }) => {
         <div>
             <div>
                 <DatePicker
-                    id="startDate"
+                    id="date-picker-id"
                     onChange={onChange}
                     selected={new Date()}
                     minDate={new Date()}
                     value={value}
                     required
-                />
-            </div>
-            <div>
-                <DatePicker
-                    id="endDate"
-                    onChange={onChange}
-                    selected={date.setDate(date.getDate()+1)}
-                    minDate={new Date()}
-                    excludeDates={[new Date(), addDays(new Date(), 0)]}
-                    required
+                    dateFormat="yyyy-MM-dd"
                 />
             </div>
         </div>
