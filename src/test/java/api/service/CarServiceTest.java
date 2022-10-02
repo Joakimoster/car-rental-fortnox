528f89c8bac89d1dae94209573f4b953225652b7
Joakim Österberg <Joakimosterberg@hotmail.com>
2022-10-02 22:07:16 +0200

    updated service test

--- src/test/java/api/service/CarServiceTest.java
+++ src/test/java/api/service/CarServiceTest.java
@@ -6,10 +6,12 @@ import com.joakim.rental.api.service.CarService;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
@@ -37,6 +39,20 @@ class CarServiceTest {
         assertNotNull(car);
     }
 
+    @Test
+    void calculateCostWhenVolvoIsSelectedCar() {
+        Car car = getCar();
+        car.setCar("Volvo");
+
+        car.setRevenue(service.calculateTotalCarCost(car.getName(), car.getEndDate(), car.getStartDate()));
+        System.out.println(car.getCar());
+        System.out.println(car.getRevenue());
+        System.out.println(car.getStartDate());
+        assertEquals(car.getRevenue(), service.calculateTotalCarCost("Volvo", car.getEndDate(), car.getStartDate()));
+    }
+
+
+
     private List<Car> getCarList() {
         List<Car> carList = new ArrayList<>();
         carList.add(getCar());
@@ -45,13 +61,19 @@ class CarServiceTest {
 
     private Car getCar() {
         Car car = new Car();
+
+        Calendar calendarStartDate = Calendar.getInstance();
+        calendarStartDate.set(2022, Calendar.NOVEMBER, 10);
+        Calendar calendarToDate = Calendar.getInstance();
+        calendarToDate.set(2022, Calendar.NOVEMBER, 14);
+
         car.setId(1L);
         car.setCar("Volvo");
         car.setName("Joakim");
         car.setAge(29);
-        car.setStartDate(new Date());
-        car.setEndDate(new Date());
-        car.setRevenue(7500);
+        car.setStartDate(calendarStartDate.getTime());
+        car.setEndDate(calendarToDate.getTime());
+        car.setRevenue(service.calculateTotalCarCost(car.getCar(), car.getEndDate(), car.getStartDate()));
         return car;
     }
 }
